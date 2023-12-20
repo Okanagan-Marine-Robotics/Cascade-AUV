d41ca5f34e0f15014f12293469c2619824a452f7
Eryk Halicki <erykhalicki0@gmail.com>
2023-12-19 16:48:33 -0800

    todo's and node packages
    Former-commit-id: 8f5ad906c2fbfc0929d6de50d7be3caec4a2f1f3

--- ros2_ws/launch/start.sh
+++ ros2_ws/launch/start.sh
@@ -1,4 +1,4 @@
 source /opt/ros/iron/setup.bash
 source ../install/local_setup.bash
-export GZ_SIM_SYSTEM_PLUGIN_PATH=~/Desktop/projects/current/robosub/ros/src/plug/build
+
 
---
+++ ros2_ws/src/obj_detection/obj_detection/ml.py
@@ -0,0 +1,5 @@
+def main():
+    print('Hi from obj_detection.')
+
+if __name__ == '__main__':
+    main()

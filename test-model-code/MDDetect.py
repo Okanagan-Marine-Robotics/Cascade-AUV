27f196b553e2aa9d4f33728da96df38e8950a0dc
ammaarkhan <ammaarkhan03@gmail.com>
2024-01-29 19:23:01 -0800

    add ml model testing code
    Former-commit-id: 59de528734ebb5f31a19b53b6eafa1e59da9e6d0

---
+++ test-model-code/MDDetect.py
@@ -0,0 +1,48 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+ 
+# cap = cv2.VideoCapture(1)  # For Webcam
+# cap.set(3, 1280)
+# cap.set(4, 720)
+cap = cv2.VideoCapture("demoOne.mov")  # For Video
+ 
+model = YOLO("best.pt")
+ 
+classNames = ["buoy_bootlegger", "buoy_gman", "gate_bootlegger", "gate_gman", "test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9", "test10", "test11" ]
+myColor = (0, 0, 255)
+while True:
+    success, img = cap.read()
+    results = model(img, stream=True)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            # Bounding Box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
+            w, h = x2 - x1, y2 - y1
+            # cvzone.cornerRect(img, (x1, y1, w, h))
+ 
+            # Confidence
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            # Class Name
+            cls = int(box.cls[0])
+            currentClass = classNames[cls]
+            print(currentClass)
+            if conf>0.5:
+                if currentClass =='NO-Hardhat' or currentClass =='NO-Safety Vest' or currentClass == "NO-Mask":
+                    myColor = (0, 0,255)
+                elif currentClass =='Hardhat' or currentClass =='Safety Vest' or currentClass == "Mask":
+                    myColor =(0,255,0)
+                else:
+                    myColor = (255, 0, 0)
+ 
+                cvzone.putTextRect(img, f'{classNames[cls]} {conf}',
+                                   (max(0, x1), max(35, y1)), scale=1, thickness=1,colorB=myColor,
+                                   colorT=(255,255,255),colorR=myColor, offset=5)
+                cv2.rectangle(img, (x1, y1), (x2, y2), myColor, 3)
+ 
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)

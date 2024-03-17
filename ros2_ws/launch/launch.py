3b4c400e5426540eccd430758cf2b41cf9779223
Eryk Halicki <erykhalicki0@gmail.com>
2024-03-17 12:12:58 -0700

    added parameters to PID nodes + yaml config
    Former-commit-id: 15a76ff085d30c47e4b6fce3c779b8fa525bba1a

--- ros2_ws/launch/launch.py
+++ ros2_ws/launch/launch.py
@@ -1,7 +1,14 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    pid_config = os.path.join(
+      'config',
+      'pid.yaml'
+    )
+
     return LaunchDescription([
         Node(
             package='hardware_integration',
@@ -80,7 +87,8 @@ def generate_launch_description():
                 ('/PID/XXX/target', '/PID/yaw/target'),
                 ('/PID/XXX/actual', '/PID/yaw/actual'),
                 ('/PID_correction/XXX', '/PID_correction/yaw')
-            ]
+            ],
+            parameters=[pid_config]
         ),
         Node(
             package='sensor_processing',
@@ -90,7 +98,8 @@ def generate_launch_description():
                 ('/PID/XXX/target', '/PID/pitch/target'),
                 ('/PID/XXX/actual', '/PID/pitch/actual'),
                 ('/PID_correction/XXX', '/PID_correction/pitch')
-            ]
+            ],
+            parameters=[pid_config]
         ),
         Node(
             package='sensor_processing',
@@ -100,7 +109,8 @@ def generate_launch_description():
                 ('/PID/XXX/target', '/PID/roll/target'),
                 ('/PID/XXX/actual', '/PID/roll/actual'),
                 ('/PID_correction/XXX', '/PID_correction/roll')
-            ]
+            ],
+            parameters=[pid_config]
         ),
         Node(
             package='sensor_processing',
@@ -110,7 +120,8 @@ def generate_launch_description():
                 ('/PID/XXX/target', '/PID/surge/target'),
                 ('/PID/XXX/actual', '/PID/surge/actual'),
                 ('/PID_correction/XXX', '/PID_correction/surge')
-            ]
+            ],
+            parameters=[pid_config]
         ),
         Node(
             package='sensor_processing',
@@ -120,7 +131,8 @@ def generate_launch_description():
                 ('/PID/XXX/target', '/PID/sway/target'),
                 ('/PID/XXX/actual', '/PID/sway/actual'),
                 ('/PID_correction/XXX', '/PID_correction/sway')
-            ]
+            ],
+            parameters=[pid_config]
         ),
         Node(
             package='sensor_processing',
@@ -130,7 +142,8 @@ def generate_launch_description():
                 ('/PID/XXX/target', '/PID/heave/target'),
                 ('/PID/XXX/actual', '/PID/heave/actual'),
                 ('/PID_correction/XXX', '/PID_correction/heave')
-            ]
+            ],
+            parameters=[pid_config]
         ),
         Node(
             package='sensor_processing',
--- ros2_ws/src/sensor_processing/sensor_processing/PIDController.py
+++ ros2_ws/src/sensor_processing/sensor_processing/PIDController.py
@@ -6,6 +6,16 @@ from message_filters import ApproximateTimeSynchronizer, Subscriber
 class PIDNode(Node):
     def __init__(self):
         super().__init__ ("generic_PID_controller")
+        self.declare_parameter('bias', 0.0)
+        self.declare_parameter('kD', 0.1)
+        self.declare_parameter('kI', 0.01)
+        self.declare_parameter('kP', 1.0)
+        self.kD=0.1
+        self.kI=0.01
+        self.kP=1.0
+        self.bias=0.0
+        self.paramsRead=False
+
         queue_size=20
         acceptable_delay=0.1 #this is how many seconds of difference we allow between the 2 subscriptions before theyre considered not matching
         tss = ApproximateTimeSynchronizer(
@@ -18,6 +28,12 @@ class PIDNode(Node):
         self.publisher_ = self.create_publisher(SensorReading, '/PID_correction/XXX', 10)
 
     def synced_callback(self, target_msg, actual_msg):
+        if(self.paramsRead==False):
+             self.kI = self.get_parameter('kI').get_parameter_value().double_value
+             self.kP = self.get_parameter('kP').get_parameter_value().double_value
+             self.kD = self.get_parameter('kD').get_parameter_value().double_value
+             self.bias = self.get_parameter('bias').get_parameter_value().double_value
+             self.paramsRead=True
         msg=SensorReading()
         #add PID code
         msg.header.stamp=self.get_clock().now().to_msg()

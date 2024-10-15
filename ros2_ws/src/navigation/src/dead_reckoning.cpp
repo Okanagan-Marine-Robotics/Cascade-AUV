51829a47d9b48768122ac36d22dc1a1a08b53352
Ubuntu <ubuntu@ip-172-26-5-49.tail2d45eb.ts.net>
2024-10-15 23:27:14 +0000

    typo fixes

--- ros2_ws/src/navigation/src/dead_reckoning.cpp
+++ ros2_ws/src/navigation/src/dead_reckoning.cpp
@@ -1,26 +1,25 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "cascade_msgs/msg/sensor_reading.hpp"
+#include "sensor_msgs/msg/imu.hpp"
 
-typedef unsigned uint32;
+typedef unsigned int uint32;
+using std::placeholders::_1;
 
-//create node 
 class DeadReckoningNode : public rclcpp::Node{
     public:
-        float x,y,z,roll,pitch,yaw;
+        float x,y,z,roll=0,pitch=0,yaw=0;
 	float last_time;
-        
-
-	DeadReckoning() : Node("deadreckoning_node" {
-		subscription_ = this->create_subscription<sensor_msgs::msg::Imu>("/camera/gyro", 10, std::bind(&DeadReckoningNode::topic_callback, this, _1));
-		roll_publisher = this->create_publisher<cascade_msgs::msg::SensorReadings()>("/roll", 10);
-		pitch_publisher_ = this->create_publisher<cascade_msgs::msg::SensorReadings()>("/pitch", 10);
-		yaw_publisher_ = this->create_publisher<cascade_msgs::msg::SensorReadings()>("/yaw", 10);
+	DeadReckoningNode() : Node("dead_reckoning_node") {
+		gyro_subscription = this->create_subscription<sensor_msgs::msg::Imu>("/camera/gyro", 10, std::bind(&DeadReckoningNode::imu_callback, this, _1));
+		roll_publisher = this->create_publisher<cascade_msgs::msg::SensorReading>("/roll", 10);
+		pitch_publisher = this->create_publisher<cascade_msgs::msg::SensorReading>("/pitch", 10);
+		yaw_publisher = this->create_publisher<cascade_msgs::msg::SensorReading>("/yaw", 10);
 	}
 
 	private:
-	void topic_callback(const sensor_msgs::msg::Imu &msg) const {
-		float total_time = msg.header.time.sec + msg.header.time.nanosec * (1.0 / 1'000'000'000);
+	void imu_callback(const sensor_msgs::msg::Imu &msg) {
+		float total_time = msg.header.stamp.sec + msg.header.stamp.nanosec * (1.0 / 1'000'000'000);
 		float dt = total_time - last_time;
 		last_time = total_time;
 
@@ -28,21 +27,22 @@ class DeadReckoningNode : public rclcpp::Node{
 		pitch += msg.angular_velocity.y * dt;
 		yaw   += msg.angular_velocity.z * dt;
 
-		auto roll_msg  = cascade_msgs::msg::SensorReadings();
-		auto pitch_msg = cascade_msgs::msg::SensorReadings();
-		auto yaw_msg   = cascade_msgs::msg::SensorReadings();
+		auto roll_msg  = cascade_msgs::msg::SensorReading();
+		auto pitch_msg = cascade_msgs::msg::SensorReading();
+		auto yaw_msg   = cascade_msgs::msg::SensorReading();
 
-		pitch_publisher_ -> publish(pitch_msg);
-		roll_publisher_  -> publish(roll_msg);
-		yaw_publisher_   -> publish(yaw_msg);
+        roll_msg.data=roll;
+        yaw_msg.data=yaw;
+        pitch_msg.data=pitch;
+
+		pitch_publisher -> publish(pitch_msg);
+		roll_publisher  -> publish(roll_msg);
+		yaw_publisher   -> publish(yaw_msg);
 	}
-
-
-	rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr subscription_;
-	rclcpp::Publisher<cascade_msgs::msg::String>::SharedPtr roll_publisher_;
-	rclcpp::Publisher<cascade_msgs::msg::String>::SharedPtr pitch_publisher_;
-	rclcpp::Publisher<cascade_msgs::msg::String>::SharedPtr yaw_publisher_;
-
+	rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr gyro_subscription;
+	rclcpp::Publisher<cascade_msgs::msg::SensorReading>::SharedPtr roll_publisher;
+	rclcpp::Publisher<cascade_msgs::msg::SensorReading>::SharedPtr pitch_publisher;
+	rclcpp::Publisher<cascade_msgs::msg::SensorReading>::SharedPtr yaw_publisher;
 };
 
 int main(int argc, char * argv[])

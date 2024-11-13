1cf7565ec37b3fa936e039b2af886f2d93a74ed3
dsalibian <dante.salibian@gmail.com>
2024-11-13 12:59:00 -0800

    add test.cpp

---
+++ ros2_ws/src/mapping/src/test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <random>
+#include <vector>
+#include <open3d/Open3D.h>
+#include <Eigen/Core.h>
+#include <string>
+
+using std::vector;
+using Eigen::Vector3d;
+
+vector<Vector3d> load_pointcloud(string filename) {
+    
+}
+
+vector<Vector3d> sphere_pointcloud(unsigned n, double radius) {
+    
+}
+
+vector<Vector3d> square_pointcloud(unsigned n, double w, double l, double h) {
+    
+}
+
+double fgr()
+
+int main() {
+
+
+    return 0;
+}
